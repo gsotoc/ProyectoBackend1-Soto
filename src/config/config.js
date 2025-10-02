80b271b38f3d74ff49d2d20e25d3c356de3d542e
gsotoc <141038576+gsotoc@users.noreply.github.com>
2025-10-02 02:15:17 -0300

    Update config.js
    Borro el mongo uri que subí por error

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,3 @@
 export const PORT = process.env.PORT || 8080;
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://gsotoc_db_user:MjbYr2VWfa09InHu@cluster0.rvhcww1.mongodb.net/test";
+export const MONGODB_URI = process.env.MONGODB_URI;
+
