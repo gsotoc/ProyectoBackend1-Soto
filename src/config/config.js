3abdfcbfed2beb5b324f28f4de83f31fd6e28328
gsotoc <141038576+gsotoc@users.noreply.github.com>
2025-10-02 02:41:14 -0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,7 @@
-export const PORT = process.env.PORT || 8080;
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = process.env.PORT || 8080;
+
 
